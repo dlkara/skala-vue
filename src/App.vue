d1bc0c6e9dd4cb1ff32c0744fb5f8623a79fd680
이현정 <lhj020731@gmail.com>
2026-07-31 16:41:19 +0900

    practice vue directive

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <script setup>
 import SampleOne from './components/practices/basic/SampleOne.vue'
 import SampleTwo from './components/practices/basic/SampleTwo.vue'
+import VueHtml from './components/practices/basic/VueHtml.vue'
+import VueHtmlXss from './components/practices/basic/VueHtmlXss.vue'
+import VueText from './components/practices/basic/VueText.vue'
+import VueBind from './components/practices/basic/VueBind.vue'
+import VueBindClass from './components/practices/basic/VueBindClass.vue'
+import VueBindStyle from './components/practices/basic/VueBindStyle.vue'
+import VueBindShorthand from './components/practices/basic/VueBindShorthand.vue'
+import VueIfElse from './components/practices/basic/VueIfElse.vue'
+import VueShow from './components/practices/basic/VueShow.vue'
+import VueFor from './components/practices/basic/VueFor.vue'
+import VuePre from './components/practices/basic/VuePre.vue'
+import VueCloak from './components/practices/basic/VueCloak.vue'
+import VueOnce from './components/practices/basic/VueOnce.vue'
+import VueMemo from './components/practices/basic/VueMemo.vue'
+import EventBasic from './components/practices/basic/EventBasic.vue'
+import EventObject from './components/practices/basic/EventObject.vue'
+import EventModifier from './components/practices/basic/EventModifier.vue'
+import ModelForm from './components/practices/basic/ModelForm.vue'
+import ModelModifier from './components/practices/basic/ModelModifier.vue'
+import VueStyle from './components/practices/basic/VueStyle.vue'
 </script>
 
 <template>
   <div style="padding: 20px">
     <SampleOne />
     <SampleTwo />
+    <VueHtml />
+    <VueHtmlXss />
+    <VueText />
+    <VueBind />
+    <VueBindClass />
+    <VueBindStyle />
+    <VueBindShorthand />
+    <VueIfElse />
+    <VueShow />
+    <VueFor />
+    <VuePre />
+    <VueCloak />
+    <VueOnce />
+    <VueMemo />
+    <EventBasic />
+    <EventObject />
+    <EventModifier />
+    <ModelForm />
+    <ModelModifier />
+    <VueStyle />
   </div>
 </template>
